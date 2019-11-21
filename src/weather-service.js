7efe06fa196dbaee3522c86d699409bac54872a4
Student <example@example.com>
2019-11-20 16:48:15 -0800

    add giphy API key and rename all keys.

--- src/weather-service.js
+++ src/weather-service.js
@@ -1,7 +1,7 @@
 export class WeatherService {
   async getWeatherByCity(city) {
     try {
-      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.openWeather}`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
